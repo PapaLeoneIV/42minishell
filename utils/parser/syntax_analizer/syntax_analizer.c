db81e6838a3517695ebf27b6c957aa8759851985
PapaLeoneIV <riccardo.leone97@gmail.com>
2024-05-23 22:54:35 +0200

    push ot

--- utils/parser/syntax_analizer/syntax_analizer.c
+++ utils/parser/syntax_analizer/syntax_analizer.c
@@ -15,6 +15,29 @@
  * double quotes, posso rendere tutte i vari token dei WORD_TOKEN generici
  * per il momento.
 */
+int check_for_non_valid_char_list(t_token *ptr, char *non_valid_char)
+{
+    int i;
+
+    while(ptr)
+    {
+        i = 0;
+        if (ptr->type == DOUBLE_QUOTES_TOKEN || ptr->type == SING_QUOTES_TOKEN 
+        || ptr->type == HERDOC_FILENAME_WITHQUOTES)
+        {
+            ptr = ptr->next;
+            continue;   
+        }
+        while(ptr->value[i] != '\0')
+        {
+            if(ft_strchr(non_valid_char, ptr->value[i]))
+                return (SUCCESS);
+            i++;
+        }
+        ptr = ptr->next;
+    }
+    return (ERROR);
+}
 
 void change_non_special_tokens_to_word_tokens(t_token *head)
 {
@@ -29,26 +52,6 @@ void change_non_special_tokens_to_word_tokens(t_token *head)
 			ptr->type = WORD_TOKEN;
 		ptr = ptr->next;
 	}
-}
-
-int headle_heredoc_syntax_error_tokens(t_token *ptr)
-{
-    if(ptr->next && (ptr->next->type == WORD_TOKEN || ptr->next->type == HERDOC_FILENAME_WITHQUOTES))
-        return (SUCCESS);
-    return (ERROR);
-}
-int handle_redirout_synt_error_tokens(t_token *ptr)
-{
-    t_token *tmp;
-
-    tmp = ptr;
-    if (tmp->next == NULL)
-        return (ERROR);
-    else if(tmp->prev && tmp->prev->type == WORD_TOKEN && ft_isnumber(tmp->prev->value))
-        return (ERROR);
-    else if (tmp->next->type == WORD_TOKEN)
-        return (SUCCESS);
-    return (ERROR);
 }
 
 /**Ugly lo so, ma mi serve per rimuovere i whitespace dalla
--- utils/parser/syntax_analizer/syntax_analizer_helpers.c
+++ utils/parser/syntax_analizer/syntax_analizer_helpers.c
@@ -8,38 +8,34 @@ int handle_pipe_synt_error_tokens(t_token *ptr)
 {
     if(ptr->next == NULL || ptr->prev == NULL)
         return (ERROR);
-    if (ptr->prev->type != WORD_TOKEN && ptr->prev->type != HERDOC_FILENAME_WITHQUOTES)
-            return (ERROR);
-    if(ptr->next->type == WORD_TOKEN || ptr->prev->type == HERDOC_FILENAME_WITHQUOTES)
-        return (SUCCESS);
-    return (ERROR);
+    if(ptr->next->type != WORD_TOKEN && ptr->next->type != HERDOC_FILENAME_WITHQUOTES
+    && ptr->next->type != GREATER_TOKEN && ptr->next->type != LESSER_TOKEN
+    && ptr->next->type != REDIR_OUT_TOKEN && ptr->next->type != HEREDOC_TOKEN)
+        return (ERROR);
+    return (SUCCESS);
 }
 
 /**Se all interno della stringa che ci viene passata e' presente uno dei caratteri
  * all interno della variabile non_valid_char posso ritornare un errore
 */
 
-int check_for_non_valid_char_list(t_token *ptr, char *non_valid_char)
+int headle_heredoc_syntax_error_tokens(t_token *ptr)
 {
-    int i;
+    if(ptr->next && (ptr->next->type == WORD_TOKEN || ptr->next->type == HERDOC_FILENAME_WITHQUOTES))
+        return (SUCCESS);
+    return (ERROR);
+}
+int handle_redirout_synt_error_tokens(t_token *ptr)
+{
+    t_token *tmp;
 
-    while(ptr)
-    {
-        i = 0;
-        if (ptr->type == DOUBLE_QUOTES_TOKEN || ptr->type == SING_QUOTES_TOKEN 
-        || ptr->type == HERDOC_FILENAME_WITHQUOTES)
-        {
-            ptr = ptr->next;
-            continue;   
-        }
-        while(ptr->value[i] != '\0')
-        {
-            if(ft_strchr(non_valid_char, ptr->value[i]))
-                return (SUCCESS);
-            i++;
-        }
-        ptr = ptr->next;
-    }
+    tmp = ptr;
+    if (tmp->next == NULL)
+        return (ERROR);
+    else if(tmp->prev && tmp->prev->type == WORD_TOKEN && ft_isnumber(tmp->prev->value))
+        return (ERROR);
+    else if (tmp->next->type == WORD_TOKEN)
+        return (SUCCESS);
     return (ERROR);
 }
 
