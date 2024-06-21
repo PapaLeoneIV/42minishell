248f44f8a120712739c7274a882e9953050a8b59
PapaLeoneIV <riccardo.leone97@gmail.com>
2024-06-21 22:53:31 +0200

    ho spostato un if vedi se va bene

--- utils/builtin/exit_build.c
+++ utils/builtin/exit_build.c
@@ -23,20 +23,19 @@ int	exit_path(t_command *cmd, t_shell *shell)
 	(void)(close(cmd->in) + close(cmd->out));
 	if (mtx_count_rows(cmd->cmd) > 2)
 		return (perror("exit\nexit: too many argument"), 300);
-	if (cmd->cmd[1])
+	while (cmd->cmd[1] && cmd->cmd[1][i++]) // qui ho spostato la prima condizione del while da un if esterno a dentro al while
 	{
-		while (cmd->cmd[1][i++])
-		{
-			if (!ft_isdigit(cmd->cmd[1][i]) || cmd->cmd[1][i] != '+' || cmd->cmd[1][i] != '-')
-				exit_status = 2;
-			if ((!ft_isdigit(cmd->cmd[1][i]) && exit_status != 2)  || cmd->cmd[1][i] != '+' || cmd->cmd[1][i] != '-')
-				perror("Alpha in exit status");
-		}
-		if (exit_status != 2)
-			exit_status = (unsigned int)ft_atoi(cmd->cmd[1]);
-		if (exit_status > 255)
-			exit_status %= 256;
+		if (!ft_isdigit(cmd->cmd[1][i]) 
+			|| cmd->cmd[1][i] != '+' || cmd->cmd[1][i] != '-')
+			exit_status = 2;
+		if ((!ft_isdigit(cmd->cmd[1][i]) && exit_status != 2)
+			|| cmd->cmd[1][i] != '+' || cmd->cmd[1][i] != '-')
+			perror("Alpha in exit status");
 	}
+	if (exit_status != 2)
+		exit_status = (unsigned int)ft_atoi(cmd->cmd[1]);
+	if (exit_status > 255)
+		exit_status %= 256;
 	clean_all(shell, 1);
 	exit(exit_status);
 }
